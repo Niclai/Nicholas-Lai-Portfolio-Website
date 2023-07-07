94692afb07323242e6fff8cd72b99a6ae3523816
Nicholas Lai <n.lai8@outlook.com>
2023-07-07 01:20:55 +0100

    finalised

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './components/projects';
 import Skills from './components/skills';
 import Contact from './components/contact';
 import Education from './components/education';
+import Experience from './components/experience';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <About />
         <Education />
         <Projects />
+        <Experience />
         <Skills />
         <Contact />
       </div>
